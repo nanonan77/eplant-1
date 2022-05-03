43b3be06c29530986adbb4824365c64b5c3d9645
champion-8 <84299332+champion-8@users.noreply.github.com>
2022-05-04 00:52:36 +0700

    Update MappingTransBaseSpec.cs

--- Sketec.Core/Specifications/Interface/Mapping9999/MappingTransBaseSpec.cs
+++ Sketec.Core/Specifications/Interface/Mapping9999/MappingTransBaseSpec.cs
@@ -13,24 +13,15 @@ namespace Sketec.Core.Specifications
         public MappingTransBaseSpec NoTracking()
         {
             Query.AsNoTracking();
-
             return this;
         }
         public MappingTransBaseSpec IncludeAll ()
         {
-            return IncludeMatch9999().IncludeMatchPlantation();
+            return IncludeMatch9999();
         }
         public MappingTransBaseSpec IncludeMatch9999()
         {
             Query.Include(b => b.Match9999s).ThenInclude(b => b.MatchDatas).ThenInclude(b => b.Mapping9999);
-
-            return this;
-        }
-        public MappingTransBaseSpec IncludeMatchPlantation()
-        {
-            Query.Include(b => b.Match9999s).ThenInclude(b => b.MatchPlantations).ThenInclude(b => b.MasterActivity).ThenInclude(b => b.MasterActivityType);
-            Query.Include(b => b.Match9999s).ThenInclude(b => b.MatchPlantations).ThenInclude(b => b.Plantation).ThenInclude(b => b.SubPlantations);
-
             return this;
         }
     }
